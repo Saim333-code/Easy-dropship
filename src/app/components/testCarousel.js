d7db4e5572fe3799b99855f99267f35ef87758ab
saif <saifsatti150@gmail.com>
2024-09-12 20:30:53 +0500

    updated carosuel2

--- src/app/components/testCarousel.js
+++ src/app/components/testCarousel.js
@@ -126,7 +126,7 @@ export default function CarouselDarkVariant() {
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="absolute inset-x-[15%] bottom-5 md:bottom-[45.5rem]  lg:bottom-[24rem]  xl:bottom-[35rem] 2xl:bottom-[46rem] py-5 text-center text-white font-serif font-extrabold md:block md:text-3xl">
               <h5 className="text-xl font-serif tracking-widest font-medium">Triple H:</h5>
-              <p className="tracking-widest font-serif font-medium">
+              <p className="tracking-widest font-serif font-medium ">
              Hum Hazir Hain.
               </p>
             </div>
